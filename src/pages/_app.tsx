6823d1e6386cf07a73b9f35e33dabd689d4e1a51
KayanHendges <kayanbhendges@gmail.com>
2021-05-06 09:44:43 -0300

    Primeiro commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/global.scss';
+
+import styles from '../styles/app.module.scss';
+
+function MyApp({ Component, pageProps }) {
+    return (
+      <div className={styles.wrapper}>
+        <Component {...pageProps} />
+      </div>
+    )
+  }
+  
+  export default MyApp
