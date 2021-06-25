0333e18d13631b8c3ee6bbaff6c4542b16b99aa3
Kayan Hendges <kayanbhendges@gmail.com>
2021-06-25 11:45:56 -0300

    sidebar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -2,16 +2,21 @@ import Link from 'next/link';
 import styles from './styles.module.scss';
 
 
+
 export default function SideBar (){
+
     return (
         <div className={styles.sideBar}>
-            <div className={styles.itemBar}>
-                <div className={styles.itemContainer}>
+            <div className={styles.itemContainer} onChange={this}>
+                <div className={styles.iconBox}>
                     <Link href="/products">
                         <a>
                             <img src="/tag1.png" alt="tag" />
                         </a>
                     </Link>
+                </div>
+                <div className={styles.itemTextContainer}>
+                    <p>Produtos</p>
                 </div>
             </div>
         </div>
