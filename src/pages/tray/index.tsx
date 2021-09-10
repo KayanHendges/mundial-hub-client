31c9353da38167b01b2cb5fd919910082e7c9d20
KayanHendges <kayanbhendges@gmail.com>
2021-09-09 22:09:27 -0300

    correção no direcionamento da pagina

--- src/pages/tray/index.tsx
+++ src/pages/tray/index.tsx
@@ -6,7 +6,7 @@ export default function tray(){
     const router = useRouter().asPath
     const routerSplit = router.split("?url=https://")
     const parameter = routerSplit[1]
-    const url = `https://${parameter}/auth.php?response_type=code&consumer_key=de64a56c6cb078828acc0d62ad9ab08c16a89440edddc28dd26ab9788ebdd10d&callback=https://wwww.mundialhub.com.br/oAuth2Tray`
+    const url = `https://${parameter}/auth.php?response_type=code&consumer_key=de64a56c6cb078828acc0d62ad9ab08c16a89440edddc28dd26ab9788ebdd10d&callback=https://www.mundialhub.com.br/oAuth2Tray`
 
     return(
         <div>
