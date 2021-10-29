561574fe320a035a6426da343c518b21bd5a0e58
Kayan Hendges <kayanbhendges@gmail.com>
2021-10-29 15:58:04 -0300

    1.0

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://api.mundialhub.com.br/'
+    baseURL: 'http://api.mundialhub.com.br/'
     // baseURL: 'http://localhost:3001/'
 })
