379046a8b439c753401d0b7420e5aa35f5544b7e
Kayan Hendges <kayanbhendges@gmail.com>
2022-05-26 11:14:41 -0300

    1.0

--- src/services/api/apiAxiosConfig.ts
+++ src/services/api/apiAxiosConfig.ts
@@ -5,8 +5,8 @@ function apiAxiosConfig(ctx?: any) {
   const { 'mundialhub.token': token } = parseCookies(ctx)
 
   const api = axios.create({
-    baseURL: 'http://192.168.0.70:3003'
-    // baseURL: 'https://api.mundialhub.com.br/v2'
+    // baseURL: 'http://192.168.0.70:3003'
+    baseURL: 'https://apiv2.mundialhub.com.br/'
   })
 
   if (token) {
